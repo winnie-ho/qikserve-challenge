4f5a0318da1ab878828ddb535425a21005948fce
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-31 16:39:36 +0000

    Creating a product card.

---
+++ src/components/productCard/productCard.vue
@@ -0,0 +1,25 @@
+<template src="./productCard.html"></template>
+<style scoped src='./productCard.css'></style>
+
+<script src>
+  export default {
+    name: 'productCard',
+    data () {
+      return {
+      }
+    },
+    props: [ "product" ],
+    methods: {
+      addToBasket(product){
+        console.log('addToBasket', product.name)
+      },
+      productPrice(price){
+        return `£${price/100}`;
+      }
+    },
+    computed: {
+
+    }
+  }
+</script>
+
