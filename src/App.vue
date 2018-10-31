e5e62042e5be2103160b23986bb6d54b3ced1438
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-31 11:31:05 +0000

    Adding the home page.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import products from './pages/products/products.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'products': products,
+  }
 }
 </script>
 
---
+++ src/pages/home/home.vue
@@ -0,0 +1,24 @@
+<template src='./home.html'></template>
+<style scoped src='./home.css'></style>
+
+<script>
+export default {
+  name: "home",
+  components: {
+
+  },
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  }
+};
+</script>
+
