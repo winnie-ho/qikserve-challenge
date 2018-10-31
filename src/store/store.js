8f7f368abaa1c3c96c14d5bbc12bd258fb31e275
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-10-31 12:03:51 +0000

    Adding vue-resource.

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+
 
 Vue.use(Vuex)
+Vue.use(VueResource)
+
 
 export const store = new Vuex.Store({
   state: {
