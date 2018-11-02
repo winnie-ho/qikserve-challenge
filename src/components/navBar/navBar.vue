9e2b95a3cc5e492c75ab0f4299434bbca62c5d88
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-11-02 11:30:14 +0000

    Using basketSize in global renderData behaviour.

--- src/components/navBar/navBar.vue
+++ src/components/navBar/navBar.vue
@@ -2,8 +2,11 @@
 <style scoped src='./navBar.css'></style>
 
 <script>
+import renderData from "../../mixins/renderData.js"
+
 export default {
   name: "navBar",
+  mixins: [ renderData ],
   components: {
 
   },
--- src/mixins/renderData.js
+++ src/mixins/renderData.js
@@ -6,6 +6,9 @@ export default {
   methods: {
     renderPrice: (price) => {
       return `£${ (price/100).toFixed(2) }`;
+    },
+    renderBasketSize: (basket) => {
+      return basket.reduce((total, item) => total + item.quantity, 0);
     }
   }
 }
