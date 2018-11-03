f72350f03f55787017e49fbc5933db6898ad6257
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-11-03 11:22:23 +0000

    Adding the products as would be from call.

--- test/unit/testHelper.js
+++ test/unit/testHelper.js
@@ -1,5 +1,26 @@
 export const mockState = {
-  products: [],
+  products: [
+    { 
+      id: "Dwt5F7KAhi",
+      name: "Amazing Pizza!",
+      price: 1099
+    }, 
+    { 
+      id: "PWWe3w1SDU",
+      name: "Amazing Burger!",
+      price: 999
+    }, 
+    {
+      "id": "C8GDyLrHJb",
+      name: "Amazing Salad!",
+      price: 499
+    }, 
+    {
+      "id": "4MB7UfpTQs",
+      name: "Boring Fries!",
+      price: 199
+    }
+  ],
   basket: [
     { 
       id:"Dwt5F7KAhi",
