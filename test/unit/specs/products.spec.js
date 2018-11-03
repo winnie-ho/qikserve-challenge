870a38b3e1c82db29b87f87ea5e8500682e4c8e6
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-11-03 12:41:53 +0000

    Adding visibility of Your order summary to the test.

--- test/unit/specs/products.spec.js
+++ test/unit/specs/products.spec.js
@@ -43,5 +43,18 @@ describe('products.vue', () => {
     expect(productsComponent.vm.basket.length).toEqual( 3 );
     expect(productsComponent.vm.basket[2].name).toEqual( 'Boring Fries!' );
   })
+
+  it('Renders the products to the "Your order" summary when items are in the basket', () => {
+    expect(productsComponent.text()).toContain('Amazing Pizza!')
+    expect(productsComponent.text()).toContain('Amazing Burger!')
+    expect(productsComponent.text()).toContain('Boring Fries!')
+    expect(productsComponent.text()).not.toContain('Amazing Salad!')
+    expect(productsComponent.find('h2').isVisible()).toBe(true)
+  })
+
+  it('Only shows the "Your order" summary when there is at least one item in the basket', () => {
+    store.state.basket = [];
+    expect(productsComponent.find('h2').isVisible()).toBe(false)
+  })
 })
 
