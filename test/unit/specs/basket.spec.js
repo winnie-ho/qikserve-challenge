09e0134f621749e22e5abc7fb4ff643be12d293d
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-11-03 16:25:00 +0000

    Tidy up of basket unit tests.

---
+++ test/unit/specs/basket.spec.js
@@ -0,0 +1,52 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import basket from '@/pages/basket/basket'
+import Vuex from 'vuex'
+import { mockState } from '../testHelper.js'
+
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('basket.vue', () => {
+  let actions;
+  let store;
+  let basketComponent;
+  
+  beforeEach(() => {
+    actions = {
+      emptyBasket: jest.fn()
+    }
+    store = new Vuex.Store({
+      state: {
+        basket: mockState.basket,
+        products: mockState.products
+      },
+      actions
+    })
+    basketComponent = shallowMount(basket, { store, localVue });
+  })
+
+  
+  it('Computed basket value returns the state basket', () => {
+    expect(basketComponent.vm.basket.length).toEqual( 3 );
+    expect(basketComponent.vm.basket[2].id).toEqual( "4MB7UfpTQs" );
+  })
+
+
+  it('Computed basketTotal value calculates the sum of the basket contents', () => {
+    expect(basketComponent.vm.basketTotal).toEqual( 16185 );
+  })
+
+
+  it('Computed basketIsEmpty returns true if the basket is empty', () => {
+    expect(basketComponent.vm.basketIsEmpty).toEqual( false );
+  })
+
+
+  it('The store action to emptyBasket is triggered when "Empty basket" button is pressed', () => {    
+    basketComponent.find('button').trigger('click');
+    expect(actions.emptyBasket).toHaveBeenCalled();
+
+  })
+})
+
