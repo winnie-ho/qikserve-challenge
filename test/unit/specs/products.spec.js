a72e7fdb3cd5971f2cbdc72cf52a1db5d2230101
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-11-03 12:02:25 +0000

    Tidying up test file using beforeEach for the mounted component.

--- test/unit/specs/products.spec.js
+++ test/unit/specs/products.spec.js
@@ -1,5 +1,4 @@
-import Vue from 'vue'
-import { shallowMount, createLocalVue, mount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import products from '@/pages/products/products'
 import Vuex from 'vuex'
 import { mockState } from '../testHelper.js'
@@ -11,6 +10,7 @@ localVue.use(Vuex)
 describe('products.vue', () => {
   let actions;
   let store;
+  let productsComponent;
   
   beforeEach(() => {
     actions = {
@@ -23,6 +23,7 @@ describe('products.vue', () => {
       },
       actions
     })
+    productsComponent = shallowMount(products, { store, localVue });
   })
 
   
@@ -32,19 +33,15 @@ describe('products.vue', () => {
   })
 
 
-  it('Computed basket value returns the state products', () => {
-    const productsComponent = shallowMount(products, { store, localVue });
+  it('Computed products value returns the state products', () => {
     expect(productsComponent.vm.products.length).toEqual( 4 );
     expect(productsComponent.vm.products[0].id).toEqual( "Dwt5F7KAhi" );
   })
 
 
   it('Computed basket value returns the state basket', () => {
-    const productsComponent = shallowMount(products, { store, localVue });
     expect(productsComponent.vm.basket.length).toEqual( 3 );
     expect(productsComponent.vm.basket[2].name).toEqual( 'Boring Fries!' );
   })
-
-
 })
 
